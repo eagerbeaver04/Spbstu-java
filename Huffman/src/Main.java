fa03df1a91795fc6791c03d72854dfca43f5c6f9
eagerbeaver04 <mike.markov04@gmail.com>
2023-12-17 14:38:49 +0300

    Update structure

--- Huffman/src/Main.java
+++ Huffman/src/Main.java
@@ -1,48 +1,53 @@
-import Classes.Huffman.*;
+import java.io.File;
+import java.util.Arrays;
 
-import java.io.IOException;
-
-
-public class Main
-{
-    public static void main(String[] args)
+public class Main {
+  public static void main(String[] args) {
+    String helpString = "Arguments format: <command> <file path>," +
+            " where command is encode, decode, info";
+    if (args.length < 2)
     {
-        if (args.length < 2)
-        {
-            System.out.println("Incorrect request in Huffman/src folder");
-            System.out.println("Use: java Main <encode/decode/info> <inputFile>");
-            return;
-        }
-
-        String mode = args[0].toLowerCase();
-        String inputFile = args[1];
-        try
-        {
-            switch (mode)
-            {
-                case "encode" ->
-                {
-                    HuffmanEncoder huffman = new HuffmanEncoder();
-                    huffman.encode(inputFile);
-                }
-                case "decode" ->
-                {
-                    HuffmanDecoder huffman = new HuffmanDecoder();
-                    huffman.decode(inputFile);
-                }
-                case "info" ->
-                {
-                    HuffmanInfo huffman = new HuffmanInfo();
-                    huffman.getInfo(inputFile);
-                    huffman.printInfo();
-                }
-                default ->
-                        System.out.println("Invalid mode. Use 'encode', 'decode', or 'info'");
-            }
-        }
-        catch (IOException e)
-        {
-            System.out.println("Error: " + e.getMessage());
-        }
+        System.out.println("Incorrect request in Huffman/src folder");
+        System.out.println("Use: java Main <encode/decode/info> <inputFile>");
+        return;
     }
-}
+    Huffman h = new Huffman();
+
+    String[] nargs = new String[args.length];
+    System.arraycopy(args, 0, nargs, 0, args.length);
+
+    String[] commands = new String[] { "encode", "decode", "info" };
+
+      switch (nargs[0]) {
+          case "decode" -> {
+
+              ArchiveData archiveData = FileManager.readArchive(args[1]);
+
+              byte[] result = h.decode(archiveData.compressedData, archiveData.recoveryMap);
+
+              FileManager.writeFile(result, archiveData.originalFileName);
+          }
+          case "encode" -> {
+
+              byte[] data = FileManager.readFile(nargs[1]);
+              byte[] result = h.encode(data);
+              File f = new File(nargs[1]);
+              String originalFileName = f.getName();
+              String outputFilePath = originalFileName.substring(0, originalFileName.lastIndexOf('.')) + ".arc";
+
+              FileManager.writeArchive(result, h.codeMap, originalFileName, outputFilePath);
+          }
+          case "info" -> {
+
+              ArchiveData archiveData = FileManager.readArchive(args[1]);
+              byte[] result = h.decode(archiveData.compressedData, archiveData.recoveryMap);
+
+              FileManager.test(archiveData, result);
+          }
+          default ->
+                  System.out.println("Invalid mode. Use 'encode', 'decode'," +
+                          " or 'info'");
+      }
+  }
+
+};
